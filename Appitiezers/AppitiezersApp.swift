2efeb64e67f54cf081fbd5728080c9f7e6ec1c7f
farthestmage <arnavkamboj511@gmail.com>
2025-04-05 18:09:01 +0530

    Initial Commit

---
+++ Appitiezers/AppitiezersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppitiezersApp.swift
+//  Appitiezers
+//
+//  Created by Arnav Kamboj on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AppitiezersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appitiezers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appitiezers
+//
+//  Created by Arnav Kamboj on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
